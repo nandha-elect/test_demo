c3257b51bd52e857f0c42ec8629d109c22365a4a
unknown <nandha.elect@gmail.com>
2020-09-30 22:49:09 +0530

    Touch Input/Mouse Input

--- main.py
+++ main.py
@@ -1,10 +1,20 @@
 import kivy
 from kivy.app import App
-from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.widget import Widget
+
+class Touch(Widget):
+    def on_touch_down(self, touch):
+        print("Mouse Down ",touch)
+
+    def on_touch_move(self, touch):
+        print("Mouse Move ",touch)
+
+    def on_touch_up(self, touch):
+        print("Mouse Up ", touch)
 
 class MyApp(App):
     def build(self):
-        return FloatLayout()
+        return Touch()
 
 if __name__ == "__main__":
     MyApp().run()
