76b9f87046ee7e238c888cc48741b80965a99441
unknown <nandha.elect@gmail.com>
2020-10-02 00:10:01 +0530

    Navigation Between Multiple Screens

--- main.py
+++ main.py
@@ -1,37 +1,22 @@
 import kivy
 from kivy.app import App
-from kivy.uix.widget import Widget
-from kivy.graphics import Rectangle
-from kivy.graphics import Color
-from kivy.graphics import Line
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
-class Touch(Widget):
-    def __init__(self, **kwargs):
-        super(Touch, self).__init__(**kwargs)
+class MainWindow(Screen):
+    pass
 
-        with self.canvas:
-            Color(1, 1, 0, 0.5, mode="rgba")
-            Line(points=(20,30,400,500,60,500))
-            Color(1, 0, 0, .5, mode='rgba')
-            self.rect = Rectangle(pos=(0,0), size=(50,50))
-            #Color(1, 1, 0, 0.5, mode="rgba")
-            #self.rect = Rectangle(pos=(200,300), size=(100,50))
+class SecondWindow(Screen):
+    pass
 
-    def on_touch_down(self, touch):
-        self.rect.pos = touch.pos
-        print("Mouse Down ",touch)
+class WindowManager(ScreenManager):
+    pass
 
-    def on_touch_move(self, touch):
-        self.rect.pos = touch.pos
-        print("Mouse Move ",touch)
-
-    def on_touch_up(self, touch):
-        self.rect.pos = touch.pos
-        print("Mouse Up ", touch)
+kv = Builder.load_file("my.kv")
 
 class MyApp(App):
     def build(self):
-        return Touch()
+        return kv
 
 if __name__ == "__main__":
     MyApp().run()
