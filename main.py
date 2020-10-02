f7ee3ca0be71654822866f7ca466543db14e74de
unknown <nandha.elect@gmail.com>
2020-10-02 23:16:40 +0530

    Creating a Popup Window

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
 import kivy
 from kivy.app import App
+from kivy.uix.widget import Widget
 from kivy.lang import Builder
-from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.floatlayout import FloatLayout
+from kivy.uix.popup import Popup
 
-class MainWindow(Screen):
+class Widgets(Widget):
+    def btn(self):
+        show_popup()
+
+class P(FloatLayout):
     pass
-
-class SecondWindow(Screen):
-    pass
-
-class WindowManager(ScreenManager):
-    pass
-
-kv = Builder.load_file("my.kv")
 
 class MyApp(App):
     def build(self):
-        return kv
+        return Widgets()
+
+def show_popup():
+    show = P()
+    popupWindow = Popup(title="Popup Window", content=show, size_hint=(None,None), size=(400,400))
+    popupWindow.open()
 
 if __name__ == "__main__":
     MyApp().run()
